b19436ad3c648d11571089a302b011891d8d67d3
Graham Wheeler <gramster@users.noreply.github.com>
2022-02-02 16:00:00 -0800

    Add a GH action to run tests using pyright (#141)

---
+++ tests/pandas/test_testing.py
@@ -0,0 +1,13 @@
+from pandas._testing import assert_series_equal
+
+import pandas as pd
+
+
+def test_types_assert_series_equal() -> None:
+    s1 = pd.Series([0, 1, 1, 0])
+    s2 = pd.Series([0, 1, 1, 0])
+    assert_series_equal(left=s1, right=s2)
+    assert_series_equal(s1, s2, check_freq=False, check_categorical=True, check_flags=True,
+                        check_datetimelike_compat=True)
+    assert_series_equal(s1, s2, check_dtype=True, check_less_precise=True, check_names=True)
+
