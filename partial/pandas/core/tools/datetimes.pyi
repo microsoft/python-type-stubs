de41d66c76d07d8de495d3384d8cf73c6352eaa1
Irv Lustig <irv@princeton.com>
2022-03-15 13:48:23 -0400

    pandas.to_datetime and pandas.to_timedelta (#173)
    * timefunc changes
    
    * add type check for subtraction
    
    * add series operators for various timedelta ops
    
    * update tests for timefuncs
    
    * Add TimedeltaIndex as a result for to_datetime

--- partial/pandas/core/tools/datetimes.pyi
+++ partial/pandas/core/tools/datetimes.pyi
@@ -1,60 +1,89 @@
 from datetime import datetime as datetime
 from numpy import datetime64 as datetime64
+import numpy as np
 
-from pandas._typing import ArrayLike as ArrayLike, Index as Index
+from pandas._typing import (
+    ArrayLike as ArrayLike,
+    Index as Index,
+    AnyArrayLike as AnyArrayLike,
+    DateTimeErrorChoices as DateTimeErrorChoices,
+    ExtensionArray as ExtensionArray,
+    Timestamp as Timestamp,
+)
 from pandas.core.dtypes.generic import ABCSeries as ABCSeries
 from pandas.core.generic import NDFrame as NDFrame
 from pandas.core.indexes.datetimes import DatetimeIndex as DatetimeIndex
-from pandas.core.frame import Series as Series, DataFrame as DataFrame
-from pandas._libs.tslibs import Timestamp
-from typing import Optional, TypeVar, Union, overload
+from pandas.core.frame import DataFrame as DataFrame
+from pandas.core.series import Series as Series
+from typing import List, Optional, Tuple, TypedDict, Union, overload
 
-ArrayConvertible = Union[list, tuple, ArrayLike, ABCSeries]
-Scalar = Union[int, float, str, Timestamp]
-DatetimeScalar = TypeVar("DatetimeScalar", Scalar, datetime, datetime64)
-DatetimeScalarOrArrayConvertible = Union[DatetimeScalar, list, tuple, ArrayLike, Series]
+ArrayConvertible = Union[List, Tuple, AnyArrayLike]
+Scalar = Union[int, float, str]
+DatetimeScalar = Union[Scalar, datetime]
+
+DatetimeScalarOrArrayConvertible = Union[DatetimeScalar, ArrayConvertible]
+
+DatetimeDictArg = Union[List[Scalar], Tuple[Scalar, ...], AnyArrayLike]
+
+class YearMonthDayDict(TypedDict, total=True):
+    year: DatetimeDictArg
+    month: DatetimeDictArg
+    day: DatetimeDictArg
+
+class FulldatetimeDict(YearMonthDayDict, total=False):
+    hour: DatetimeDictArg
+    hours: DatetimeDictArg
+    minute: DatetimeDictArg
+    minutes: DatetimeDictArg
+    second: DatetimeDictArg
+    seconds: DatetimeDictArg
+    ms: DatetimeDictArg
+    us: DatetimeDictArg
+    ns: DatetimeDictArg
+
+DictConvertible = Union[FulldatetimeDict, "DataFrame"]
 
 def should_cache(arg: ArrayConvertible, unique_share: float = ..., check_count: Optional[int] = ...) -> bool: ...
 @overload
 def to_datetime(
-    arg: Union[DataFrame, Series],
-    errors: str = ...,
-    dayfirst: bool = ...,
-    yearfirst: bool = ...,
-    utc: Optional[bool] = ...,
-    format: Optional[str] = ...,
-    exact: bool = ...,
-    unit: Optional[str] = ...,
-    infer_datetime_format: bool = ...,
-    origin: Scalar = ...,
-    cache: bool = ...,
-) -> Series[datetime64]: ...
-@overload
-def to_datetime(
     arg: DatetimeScalar,
-    errors: str = ...,
+    errors: DateTimeErrorChoices = ...,
     dayfirst: bool = ...,
     yearfirst: bool = ...,
-    utc: Optional[bool] = ...,
-    format: Optional[str] = ...,
+    utc: bool | None = ...,
+    format: str | None = ...,
     exact: bool = ...,
-    unit: Optional[str] = ...,
+    unit: str | None = ...,
     infer_datetime_format: bool = ...,
-    origin: Scalar = ...,
+    origin=...,
     cache: bool = ...,
 ) -> Timestamp: ...
 @overload
 def to_datetime(
-    arg: Union[list, tuple, ArrayLike, Index],
-    errors: str = ...,
+    arg: Series | DictConvertible,
+    errors: DateTimeErrorChoices = ...,
     dayfirst: bool = ...,
     yearfirst: bool = ...,
-    utc: Optional[bool] = ...,
-    format: Optional[str] = ...,
+    utc: bool | None = ...,
+    format: str | None = ...,
     exact: bool = ...,
-    unit: Optional[str] = ...,
+    unit: str | None = ...,
     infer_datetime_format: bool = ...,
-    origin: Scalar = ...,
+    origin=...,
+    cache: bool = ...,
+) -> Series[Timestamp]: ...
+@overload
+def to_datetime(
+    arg: list | tuple | np.ndarray | Index | ExtensionArray,
+    errors: DateTimeErrorChoices = ...,
+    dayfirst: bool = ...,
+    yearfirst: bool = ...,
+    utc: bool | None = ...,
+    format: str | None = ...,
+    exact: bool = ...,
+    unit: str | None = ...,
+    infer_datetime_format: bool = ...,
+    origin=...,
     cache: bool = ...,
 ) -> DatetimeIndex: ...
 def to_time(arg, format=..., infer_time_format: bool = ..., errors: str = ...): ...
---
+++ tests/pandas/test_timefuncs.py
@@ -0,0 +1,94 @@
+# flake8: noqa: F841
+
+import pandas as pd
+import datetime as dt
+
+from pandas.testing import assert_series_equal
+
+from . import check_series_result
+
+
+def test_types_init() -> None:
+    ts: pd.Timestamp = pd.Timestamp("2021-03-01T12")
+    ts1: pd.Timestamp = pd.Timestamp(dt.date(2021, 3, 15))
+    ts2: pd.Timestamp = pd.Timestamp(dt.datetime(2021, 3, 10, 12))
+    ts3: pd.Timestamp = pd.Timestamp(pd.Timestamp("2021-03-01T12"))
+    ts4: pd.Timestamp = pd.Timestamp(1515590000.1, unit="s")
+    ts5: pd.Timestamp = pd.Timestamp(1515590000.1, unit="s", tz="US/Pacific")
+    ts6: pd.Timestamp = pd.Timestamp(1515590000100000000)  # plain integer (nanosecond)
+    ts7: pd.Timestamp = pd.Timestamp(2021, 3, 10, 12)
+    ts8: pd.Timestamp = pd.Timestamp(year=2021, month=3, day=10, hour=12)
+    ts9: pd.Timestamp = pd.Timestamp(year=2021, month=3, day=10, hour=12, tz="US/Pacific")
+
+
+def test_types_arithmetic() -> None:
+    ts: pd.Timestamp = pd.to_datetime("2021-03-01")
+    ts2: pd.Timestamp = pd.to_datetime("2021-01-01")
+    delta: pd.Timedelta = pd.to_timedelta("1 day")
+
+    tsr: pd.Timedelta = ts - ts2
+    tsr2: pd.Timestamp = ts + delta
+    tsr3: pd.Timestamp = ts - delta
+    tsr4: pd.Timedelta = ts - dt.datetime(2021, 1, 3)
+
+
+def test_types_comparison() -> None:
+    ts: pd.Timestamp = pd.to_datetime("2021-03-01")
+    ts2: pd.Timestamp = pd.to_datetime("2021-01-01")
+
+    tsr: bool = ts < ts2
+    tsr2: bool = ts > ts2
+
+
+def test_types_pydatetime() -> None:
+    ts: pd.Timestamp = pd.Timestamp("2021-03-01T12")
+
+    datet: dt.datetime = ts.to_pydatetime()
+    datet2: dt.datetime = ts.to_pydatetime(False)
+    datet3: dt.datetime = ts.to_pydatetime(warn=True)
+
+
+def test_to_timedelta() -> None:
+    td: pd.Timedelta = pd.to_timedelta(3, "days")
+    tdi: pd.TimedeltaIndex = pd.to_timedelta([2, 3], "minutes")
+    tds: pd.Series = pd.to_timedelta(pd.Series([2, 3]), "hours")
+
+
+def test_timedelta_arithmetic() -> None:
+    td1: pd.Timedelta = pd.to_timedelta(3, "days")
+    td2: pd.Timedelta = pd.to_timedelta(4, "hours")
+    td3: pd.Timedelta = td1 + td2
+    td4: pd.Timedelta = td1 - td2
+    td5: pd.Timedelta = td1 * 4.3
+    td6: pd.Timedelta = td3 / 10.2
+
+
+def test_timedelta_series_arithmetic() -> None:
+    tds1: pd.Series = pd.to_timedelta(pd.Series([2, 3]), "minutes")
+    td1: pd.Timedelta = pd.Timedelta("2 days")
+    r1: pd.Series = tds1 + td1
+    r2: pd.Series = tds1 - td1
+    r3: pd.Series = tds1 * 4.3
+    r4: pd.Series = tds1 / 10.2
+
+
+def test_timestamp_timedelta_series_arithmetic() -> None:
+    tscheck = pd.Series([pd.Timestamp("2022-03-05"), pd.Timestamp("2022-03-06")])
+    ts1 = pd.to_datetime(pd.Series(["2022-03-05", "2022-03-06"]))
+    # ignore type on next, because `tscheck` has Unknown dtype
+    check_series_result(ts1, tscheck.dtype)  # type: ignore
+    assert_series_equal(tscheck, ts1)
+    ts2 = pd.to_datetime(pd.Series(["2022-03-08", "2022-03-10"]))
+    td1 = pd.to_timedelta([2, 3], "seconds")
+    r1 = ts1 - ts2
+    check_series_result(r1, td1.dtype)
+    r2 = r1 / td1
+    check_series_result(r2, float)
+    r3 = r1 - td1
+    check_series_result(r3, td1.dtype)
+    r4 = pd.Timedelta(5, "days") / r1
+    check_series_result(r4, float)
+    sb = pd.Series([1, 2]) == pd.Series([1, 3])
+    check_series_result(sb, bool)
+    r5 = sb * r1
+    check_series_result(r5, r1.dtype)
