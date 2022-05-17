69fc07365df788bc8f161728fb4366addbf94c47
Graham Wheeler <gram@geekraver.com>
2022-05-17 16:30:26 -0700

    Remove the pandas tests.

--- tests/pandas/test_merge.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-def test_types_merge() -> None:
-    df = pd.DataFrame(data={"col1": [1, 1, 2], "col2": [3, 4, 5]})
-    df2 = pd.DataFrame(data={"col1": [1, 1, 2], "col2": [0, 1, 0]})
-    l = ["col1", "col2"]
-    df.merge(df2, on=l)
