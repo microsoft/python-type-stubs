771105790fdfb2733ba3e0faefce25a03f994fb5
Erik De Bonte <erikd@microsoft.com>
2023-04-12 08:29:27 -0700

    Pyright use 3.11 so assert_type is supported

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,7 +2,7 @@
 
 python -m pip install --upgrade pyright
 python -m pip install -r requirements.txt
-pyright --lib --pythonversion 3.9 -p ../pyrighttestconfig.json .
+pyright --lib --pythonversion 3.11 -p ../pyrighttestconfig.json .
 
 
 
