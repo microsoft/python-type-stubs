c79d685081e0e2b57b29cf90bce93a89d193a30a
Erik De Bonte <erikd@microsoft.com>
2023-04-12 08:24:55 -0700

    Reintroduce test action

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,7 +2,6 @@
 
 python -m pip install --upgrade pyright
 python -m pip install -r requirements.txt
-cp -R ../partial/ ../typings
 pyright --lib --pythonversion 3.9 -p ../pyrighttestconfig.json .
 
 
