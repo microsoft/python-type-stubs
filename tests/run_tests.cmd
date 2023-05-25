36729805bbadfa029b418266a24b6fb47a69ec90
Erik De Bonte <erikd@microsoft.com>
2023-05-25 14:39:34 -0700

    Move stubs to new `stubs` subdirectory (#282)

--- tests/run_tests.cmd
+++ tests/run_tests.cmd
@@ -1,3 +1,3 @@
 pip install --upgrade pyright
 pip install -r requirements.txt
-pyright --lib --pythonversion 3.11 -p ../pyrighttestconfig.json .
+pyright --pythonversion 3.11 -p pyrighttestconfig.json .
