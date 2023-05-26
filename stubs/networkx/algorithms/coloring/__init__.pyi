ccd73b246729d1995b4e5b3302e6e034409072bd
lkct <liangkct@yahoo.com>
2023-05-26 21:41:58 +0100

    Remove `__all__` in `__init__.pyi` in favor of `A as A`

--- stubs/networkx/algorithms/coloring/__init__.pyi
+++ stubs/networkx/algorithms/coloring/__init__.pyi
@@ -2,5 +2,3 @@ from .greedy_coloring import greedy_color as greedy_color
 from .equitable_coloring import (
     equitable_color as equitable_color,
 )
-
-__all__ = ["greedy_color", "equitable_color"]
--- stubs/networkx/algorithms/minors/__init__.pyi
+++ stubs/networkx/algorithms/minors/__init__.pyi
@@ -5,11 +5,3 @@ from ...algorithms.minors.contraction import (
     identified_nodes as identified_nodes,
     quotient_graph as quotient_graph,
 )
-
-__all__ = [
-    "contracted_edge",
-    "contracted_nodes",
-    "equivalence_classes",
-    "identified_nodes",
-    "quotient_graph",
-]
