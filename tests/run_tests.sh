5ef4cf765373b5f123242d2d1ba8db371e01b6ed
Avasam <samuel.06@hotmail.com>
2023-11-21 18:47:02 -0500

    Automate on CI

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,7 +2,8 @@
 
 python -m pip install --upgrade pyright
 python -m pip install -r requirements.txt
+
+cd ../
+python -m isort .
+python -m black .
 pyright --pythonversion 3.11 -p pyrighttestconfig.json .
-
-
-
