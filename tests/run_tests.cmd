a03f9346e8ee9180796218b82f3f47992fef2bb1
Avasam <samuel.06@hotmail.com>
2023-12-16 14:41:49 -0500

    Keep scripts untouched

--- tests/run_tests.cmd
+++ tests/run_tests.cmd
@@ -1,5 +1,3 @@
 pip install --upgrade pyright
 pip install -r requirements.txt
-
-cd ../
 pyright --pythonversion 3.11 -p pyrighttestconfig.json .
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,5 +3,5 @@
 python -m pip install --upgrade pyright
 python -m pip install -r requirements.txt
 
-cd ../
+
 pyright --pythonversion 3.11 -p pyrighttestconfig.json .
