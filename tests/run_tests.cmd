eaf515f1ded5a93faa6b4a6d2668c6b22543c154
Avasam <samuel.06@hotmail.com>
2023-12-16 14:38:26 -0500

    PR comments

--- tests/run_tests.cmd
+++ tests/run_tests.cmd
@@ -2,6 +2,4 @@ pip install --upgrade pyright
 pip install -r requirements.txt
 
 cd ../
-isort .
-black .
 pyright --pythonversion 3.11 -p pyrighttestconfig.json .
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,6 +4,4 @@ python -m pip install --upgrade pyright
 python -m pip install -r requirements.txt
 
 cd ../
-python -m isort .
-python -m black .
 pyright --pythonversion 3.11 -p pyrighttestconfig.json .
