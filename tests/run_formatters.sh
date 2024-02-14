c04314ab46638e9f6d27276786460d2f57e33efe
Avasam <samuel.06@hotmail.com>
2024-02-13 19:55:44 -0500

    Update and fix tests to run from anywhere

--- tests/run_formatters.sh
+++ tests/run_formatters.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+test_folder=$(dirname "${BASH_SOURCE[0]}")
+root=$(dirname $test_folder)
 
 python -m pip install isort black -U
 
-cd ../
+cd $root
 # TODO: Remove --check-only and fix all
 python -m isort . --check-only
 # TODO: Remove --check and fix all
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+test_folder=$(dirname "${BASH_SOURCE[0]}")
+root=$(dirname $test_folder)
 
-python -m pip install -r requirements.txt -U
+python -m pip install -r $test_folder/requirements.txt -U
 
-cd ../
+cd $root
 python -m mypy .
 npx -y pyright@latest .
