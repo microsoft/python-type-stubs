0080da4a8fe54a0f8f0eefe114d1d0a4f5c468df
Avasam <samuel.06@hotmail.com>
2024-05-02 23:30:11 -0400

    Not installing pyright on CI

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,6 +2,7 @@
 test_folder=$(dirname "${BASH_SOURCE[0]}")
 root=$(dirname $test_folder)
 
+python -m pip install --upgrade pyright
 python -m pip install --upgrade -r $test_folder/requirements.txt
 
 cd $root
