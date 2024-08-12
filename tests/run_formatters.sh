6623bb3c76eb7e3a4a19a87d944bfba0daf8c29f
Avasam <samuel.06@hotmail.com>
2024-08-11 20:30:23 -0400

    Forgot to delete run_formatters

--- tests/run_formatters.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-test_folder=$(dirname "${BASH_SOURCE[0]}")
-root=$(dirname $test_folder)
-
-python -m pip install --upgrade isort black
-
-cd $root
-python -m isort .
-python -m black .
