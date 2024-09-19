93f655740cb9a3f97120cb8c140171ca5a3ce896
Erik De Bonte <erikd@microsoft.com>
2024-09-19 16:01:15 -0700

    Add tests for sklearn normalize overloads

---
+++ tests/sklearn/preprocessing_tests.py
@@ -0,0 +1,32 @@
+# pyright: reportUnknownVariableType=false
+# pyright: reportMissingTypeStubs=false
+
+from typing import Any, assert_type
+from numpy import ndarray
+from sklearn.preprocessing import normalize
+
+from scipy.sparse._matrix import spmatrix
+from scipy.sparse._csr import csr_matrix
+
+
+# normalize with matrix
+matrix: spmatrix = spmatrix()
+result = normalize(matrix)
+assert_type(result, csr_matrix)
+
+result = normalize(matrix, return_norm=False)
+assert_type(result, csr_matrix)
+
+result = normalize(matrix, return_norm=True)
+assert_type(result, tuple[csr_matrix, ndarray[Any, Any]])
+
+# normalize with array
+array_like = [1]
+result = normalize(array_like)
+assert_type(result, ndarray[Any, Any])
+
+result = normalize(array_like, return_norm=False)
+assert_type(result, ndarray[Any, Any])
+
+result = normalize(array_like, return_norm=True)
+assert_type(result, tuple[ndarray[Any, Any], ndarray[Any, Any]])
