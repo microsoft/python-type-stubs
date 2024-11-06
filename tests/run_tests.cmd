f33aa549e65147887356c8354f8d8d7af831dd1c
Avasam <samuel.06@hotmail.com>
2024-11-06 18:39:01 -0500

    Update automated tests, pass as-is (#301)
    * Update automated tests, pass as-is
    
    * Address PR comments
    
    * Split formatters shell script
    
    * Update and fix tests to run from anywhere
    
    * Update tests
    
    * Not installing pyright on CI
    
    * Not a TODO
    
    * Set pyright's typeCheckingMode to standard
    
    * Use Python scripts instead and check pyright against pylance-prerelease
    
    * Forgot to delete run_formatters
    
    * typing_extensions goes under "Typed libraries and stubs"
    
    * Bump minimum Python version to 3.9
    
    * Missed mypy target version
    
    * Unpin typing_extensions
    
    * post-merge fix
    
    * use docopt-ng instead of docopt
    
    * Add type-var to mypy disable

--- tests/run_tests.cmd
+++
@@ -1,3 +0,0 @@
-pip install --upgrade pyright
-pip install -r requirements.txt
-pyright --pythonversion 3.11 -p pyrighttestconfig.json .
--- tests/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python -m pip install --upgrade pyright
-python -m pip install -r requirements.txt
-
-
-pyright --pythonversion 3.11 -p pyrighttestconfig.json .
