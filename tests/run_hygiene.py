5d6b16f8c664154c73b36e46b41217d1006ccd7d
Avasam <samuel.06@hotmail.com>
2025-05-05 16:42:15 -0400

    mypy: Fix all no-untyped-call

--- tests/run_hygiene.py
+++ tests/run_hygiene.py
@@ -4,23 +4,23 @@ import sys
 from pathlib import Path
 
 
-def install_requirements():
+def install_requirements() -> None:
     print("\nInstalling requirements...")
     subprocess.check_call((sys.executable, "-m", "pip", "install", "pip>=25.1"))
     subprocess.check_call((sys.executable, "-m", "pip", "install", "--upgrade", "--group", "hygiene"))
 
 
-def run_ruff_fix():
+def run_ruff_fix() -> subprocess.CompletedProcess[bytes]:
     print("\nRunning Ruff check --fix...")
     return subprocess.run((sys.executable, "-m", "ruff", "check", "--fix"))
 
 
-def run_ruff_format():
+def run_ruff_format() -> subprocess.CompletedProcess[bytes]:
     print("\nRunning Ruff format...")
     return subprocess.run((sys.executable, "-m", "ruff", "format"))
 
 
-def main():
+def main() -> None:
     os.chdir(Path(__file__).parent.parent)
 
     install_requirements()
