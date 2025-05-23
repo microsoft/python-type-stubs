ef1437bed9b19146aa84acb769f1c702a7c8a9f8
Avasam <samuel.06@hotmail.com>
2025-05-23 14:41:02 -0400

    Update transformers-stubs

--- stubs/transformers-stubs/models/auto/tokenization_auto.pyi
+++ stubs/transformers-stubs/models/auto/tokenization_auto.pyi
@@ -1,24 +1,24 @@
 import os
 from collections import OrderedDict
 from typing import Any
+from typing_extensions import TypeAlias
 
-from transformers.configuration_utils import PretrainedConfig
 from transformers.models.auto.auto_factory import _LazyAutoMapping
 from transformers.tokenization_utils import PreTrainedTokenizer
 from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
 
 TOKENIZER_MAPPING_NAMES: OrderedDict[str, tuple[str | None, str | None]]
 TOKENIZER_MAPPING: _LazyAutoMapping
-CONFIG_TO_TYPE: dict[type[PretrainedConfig], str]
+CONFIG_TO_TYPE: dict[str, str]
 
-def tokenizer_class_from_name(class_name: str) -> PreTrainedTokenizer | PreTrainedTokenizerFast: ...
+def tokenizer_class_from_name(class_name: str) -> type[Any] | None: ...
 def get_tokenizer_config(
     pretrained_model_name_or_path: str | os.PathLike[str],
     cache_dir: str | os.PathLike[str] | None = None,
     force_download: bool = False,
-    resume_download: bool = False,
+    resume_download: bool | None = None,
     proxies: dict[str, str] | None = None,
-    use_auth_token: bool | str | None = None,
+    token: bool | str | None = None,
     revision: str | None = None,
     local_files_only: bool = False,
     subfolder: str = "",
@@ -28,7 +28,5 @@ def get_tokenizer_config(
 class AutoTokenizer:
     def __init__(self) -> None: ...
     @classmethod
-    def from_pretrained(
-        cls, pretrained_model_name_or_path: str | os.PathLike[str], *inputs, **kwargs
-    ) -> PreTrainedTokenizer | PreTrainedTokenizerFast: ...
-    def register(config_class, slow_tokenizer_class=None, fast_tokenizer_class=None) -> None: ...
+    def from_pretrained(cls, pretrained_model_name_or_path: str | os.PathLike[str], *inputs, **kwargs): ...
+    def register(config_class, slow_tokenizer_class=None, fast_tokenizer_class=None, exist_ok=False) -> None: ...
