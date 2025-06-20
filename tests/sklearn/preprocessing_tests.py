7b322c9786821badf6495c33f897937e4ffdf8f1
Avasam <samuel.06@hotmail.com>
2025-06-20 14:41:19 -0400

    Fix pyright test issue with numpy.ndarray across multiple versions of Python

--- tests/sklearn/preprocessing_tests.py
+++ tests/sklearn/preprocessing_tests.py
@@ -1,4 +1,3 @@
-from typing import Any
 from typing_extensions import assert_type
 
 from numpy import ndarray
@@ -15,15 +14,15 @@ result = normalize(matrix, return_norm=False)
 assert_type(result, csr_matrix)
 
 result = normalize(matrix, return_norm=True)
-assert_type(result, tuple[csr_matrix, ndarray[Any, Any]])
+assert_type(result, tuple[csr_matrix, ndarray])
 
 # normalize with array
 array_like = [1]
 result = normalize(array_like)
-assert_type(result, ndarray[Any, Any])
+assert_type(result, ndarray)
 
 result = normalize(array_like, return_norm=False)
-assert_type(result, ndarray[Any, Any])
+assert_type(result, ndarray)
 
 result = normalize(array_like, return_norm=True)
-assert_type(result, tuple[ndarray[Any, Any], ndarray[Any, Any]])
+assert_type(result, tuple[ndarray, ndarray])
